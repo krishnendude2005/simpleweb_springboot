1551248b546fdd1d8cb1ab54d3a61cc7279684d3
Krishnendu De <krishnendu1205.be22@chitkarauniversity.edu.in>
2025-12-20 22:04:07 +0530

    UserDetails Feature custom handling examples

--- SimpleWebApp/src/main/java/com/krishnendu/SimpleWebApp/config/SecurityConfig.java
+++ SimpleWebApp/src/main/java/com/krishnendu/SimpleWebApp/config/SecurityConfig.java
@@ -6,6 +6,10 @@ import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -36,6 +40,24 @@ public class SecurityConfig {
 //        http.csrf(custCsrf);
 //--------------------------------------------------------------------
 
+    }
 
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+
+        UserDetails user1 = User
+                .withDefaultPasswordEncoder()
+                .username("krish")
+                .password("k@123")
+                .build();
+
+        UserDetails user2 = User
+                .withDefaultPasswordEncoder()
+                .username("krishnendu")
+                .password("k@12345")
+                .build();
+
+        return new InMemoryUserDetailsManager(user1);
     }
 }
